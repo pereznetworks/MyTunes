16ce8db67b462728bd11b2b7d5e8ae4bf5b5a920
pereznetworks <34382720+pereznetworks@users.noreply.github.com>
2025-11-24 17:29:58 -0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Views;
+
+namespace MyTunes
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            /* BindingContext = new StateContainerViewModel(); // or resolve from DI/Resource*/
+        }
+
+        void OnPlayPauseButtonClicked(object sender, EventArgs args)
+        {
+            if (MyTunes.CurrentState == MediaElementState.Stopped ||
+                MyTunes.CurrentState == MediaElementState.Paused)
+            {
+                MyTunes.Play();
+            }
+            else if (MyTunes.CurrentState == MediaElementState.Playing)
+            {
+                MyTunes.Pause();
+            }
+        }
+
+        void OnStopButtonClicked(object sender, EventArgs args)
+        {
+            MyTunes.Stop();
+        }
+    }
+}
