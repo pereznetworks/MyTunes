b4be6ec0a32a3a3ce2c763de76e549af4fb6976b
pereznetworks <34382720+pereznetworks@users.noreply.github.com>
2025-11-25 16:21:19 -0800

    adding compile  options for android

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -5,24 +5,30 @@ namespace MyTunes
 {
     public static class MauiProgram
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .UseMauiCommunityToolkit()
-                .UseMauiCommunityToolkitMediaElement()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+            public static MauiApp CreateMauiApp()
+            {
+                var builder = MauiApp.CreateBuilder();
+                builder
+                    .UseMauiApp<App>()
+                    .ConfigureFonts(fonts =>
+                    {
+                        fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                        fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    });
 
-#if DEBUG
-        builder.Logging.AddDebug();
-#endif
 
-            return builder.Build();
+                    #if ANDROID
+                        builder.UseMauiCommunityToolkit()
+                               .UseMauiCommunityToolkitMediaElement();
+                    #else
+                        builder.UseMauiCommunityToolkit();
+                    #endif
+                    
+                    #if DEBUG   
+                            builder.Logging.AddDebug();
+                    #endif
+
+                return builder.Build();
         }
     }
 }
